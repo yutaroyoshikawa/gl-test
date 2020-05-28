f95a07b4c22cd91f956f76ea263d34495cb6e1bf
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-05-28 10:40:27 +0900

    canvas作成

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,19 +1,17 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
+import * as THREE from "three";
+import { useViewPort } from "../hooks/viewPort";
+import { vert, frag } from "../shaders/home";
+import { GL } from "../utils/gl";
 
-const useViewPort = () => {
-  const [ width, setWidth ] = useState(0);
-  const [ height, setHeight ] = useState(0);
+const obj = new THREE.Object3D();
+const modelMat = obj.matrixWorld;
 
-  useEffect(() => {
-    setWidth(innerWidth);
-    setHeight(innerHeight);
-  }, []);
-
-  return {
-    width,
-    height
-  };
-};
+const vertexPosition = [
+  0.0, 1.0, 0.0,
+  1.0, 0.0, 0.0,
+ -1.0, 0.0, 0.0
+];
 
 const Canvas: React.FC = () => {
   const { width, height } = useViewPort();
@@ -21,20 +19,65 @@ const Canvas: React.FC = () => {
 
   useEffect(() => {
     if (!canvasRef.current) return;
-    const gl = canvasRef.current.getContext("webgl2");
-    if (!gl) return;
-    gl.flush();
+    const ctx = canvasRef.current.getContext("webgl");
+    if (!ctx) return;
+
+    const renderingGL = async () => {
+      try {
+        ctx.clearColor(1.0, 1.0, 1.0, 1.0);
+        ctx.clearDepth(1.0);
+        ctx.clear(ctx.COLOR_BUFFER_BIT | ctx.DEPTH_BUFFER_BIT);
+      
+        const renderer = new GL(ctx);
+      
+        await renderer.initGl(vert, frag);
+        const GLProgram = renderer.program;
+        if (!GLProgram) return;
+        const attLocation = ctx.getAttribLocation(GLProgram, 'position');
+    
+        const vbo = renderer.createBuffer(vertexPosition);
+        if (!vbo) return;
+    
+        ctx.bindBuffer(ctx.ARRAY_BUFFER, vbo);
+        ctx.enableVertexAttribArray(attLocation);
+        ctx.vertexAttribPointer(attLocation, 3, ctx.FLOAT, false, 0, 0);
+    
+        const uniLocation = ctx.getUniformLocation(GLProgram, "mvpMatrix");
+        const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 2000);
+        const viewMatrix = camera.matrixWorldInverse;
+        const projectionMatrix = camera.projectionMatrix;
+        const mvpMatrix = modelMat.multiplyMatrices(viewMatrix, projectionMatrix);
+  
+        console.log(mvpMatrix.toArray());
+    
+        ctx.uniformMatrix4fv(uniLocation, false, mvpMatrix.toArray());
+        ctx.drawArrays(ctx.TRIANGLES, 0, 3);
+    
+        renderer.renderGl();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    renderingGL();
   }, []);
 
   return (
-    <canvas
-      width={width}
-      height={height}
-      ref={canvasRef}
-      style={{ width: "100vw", height: "100vh" }}
-    >
-      javascript not working.
-    </canvas>
+    <>
+      <canvas
+        width={width}
+        height={height}
+        ref={canvasRef}
+      >
+        javascript not working.
+      </canvas>
+      <style>{`
+        canvas {
+          width: 100vw;
+          height: 100vh;
+        }
+      `}</style>
+    </>
   )
 }
 
